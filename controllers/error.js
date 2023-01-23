30f18b90be83e1939811f9890c21fd1753b865c5
Qasim Ahmad Khan <qasim.ahmad.khan@techloyce.com>
2023-01-23 15:42:26 +0500

    Added validation

--- controllers/error.js
+++
@@ -1,3 +0,0 @@
-exports.get404 = (req, res) => {
-  res.status(404).render("register");
-};
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
+const { check } = require("express-validator/check");
 const router = express.Router();
 
 const userController = require("../controllers/user");
 
-router.get("/", userController.getIndex);
-
-router.post("/register", userController.register);
-router.post("/login", userController.login);
-router.get("/user",passport.authenticate('jwt', { session: false}), userController.getUser);
+router.post(
+  "/register",
+  check("email").isEmail().withMessage("Please enter a valid email"),
+  check("password", "Password has to be valid.").isLength({ min: 5 }),
+  userController.register
+);
+router.post(
+  "/login",
+  check("email").isEmail().withMessage("Please enter a valid email"),
+  check("password", "Password has to be valid.").isLength({ min: 5 }),
+  userController.login
+);
+router.get(
+  "/user",
+  passport.authenticate("jwt", { session: false }),
+  userController.getUser
+);
 
 module.exports = router;
